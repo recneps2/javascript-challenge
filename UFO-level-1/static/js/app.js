24296e73e27c0b4235b3fd4a8c74211d6821e859
Morgan <44606382+recneps2@users.noreply.github.com>
2021-02-15 14:26:50 -0500

    <updating app, event listener>

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -28,14 +28,19 @@ function handleClick() {
   const date = d3.select("#datetime").property("value");
   let filteredData = tableData;
 
-  // Check date was entered and filter the data using that date.
+  // check date was entered and filter the data using that date.
   if (date) {
     // Apply `filter` to the table data to only keep the
     // rows where the `datetime` value matches the filter value
     filteredData = filteredData.filter(row => row.datetime === date);
   }
 
-  // Rebuild the table using the filtered data... If no date entered then filteredData will just be the original tableData
+  // rebuild the table using the filtered data... If no date entered then filteredData will just be the original tableData
   buildTable(filteredData);
 }
 
+// event  listener for the form button
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// build the table when the page loads
+buildTable(tableData);
