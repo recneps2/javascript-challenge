215cc9eec4f07b050029ea6c757c1cd34abf3c91
Morgan <44606382+recneps2@users.noreply.github.com>
2021-02-15 14:16:16 -0500

    <updating app, filtering>

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -21,3 +21,21 @@ function buildTable(data) {
     );
   });
 }
+
+function handleClick() {
+
+  // grab the datetime value from the filter
+  const date = d3.select("#datetime").property("value");
+  let filteredData = tableData;
+
+  // Check date was entered and filter the data using that date.
+  if (date) {
+    // Apply `filter` to the table data to only keep the
+    // rows where the `datetime` value matches the filter value
+    filteredData = filteredData.filter(row => row.datetime === date);
+  }
+
+  // Rebuild the table using the filtered data... If no date entered then filteredData will just be the original tableData
+  buildTable(filteredData);
+}
+
